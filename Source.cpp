d72c92c4f4bef0d298505ba0ef92596427dfc2ac
gboecker24 <g-boecker@onu.edu>
2024-10-29 21:32:04 -0400

    Completed countCharacter function.

--- Source.cpp
+++ Source.cpp
@@ -1,2 +1,30 @@
 // Lab8-1 10/24/2024   Gavin Boecker
 // Initialized Visual Studio Project
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int countCharacter(string str) {
+	
+	int num = str.length();
+	return num;
+
+}
+
+
+int main(){
+	
+	string word;
+	
+	cout << "Enter a word: ";
+	cin >> word;
+
+	cout << endl << countCharacter(word);
+
+	
+}
