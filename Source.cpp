35fc07615893de567c6f6f345fc04c517786e775
gboecker24 <g-boecker@onu.edu>
2024-10-30 12:25:02 -0400

    Completed countVowel function

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,33 @@
 // Lab8-1 10/24/2024   Gavin Boecker
 // Initialized Visual Studio Project
 
+
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
 
-int countCharacter(string str) {
+int countVowel(string str) {
 	
-	int num = str.length();
-	return num;
+	int length = str.length();
+	int vowels = 0;
+	
+	for (int i = 0; i < length; i++)
+	{
+		char letter = str[i];
 
+		if (letter == 'a' || letter == 'A' || letter == 'E' || letter == 'e' || letter == 'O' || letter == 'o' || letter == 'I' || letter == 'i' || letter == 'U' || letter == 'u'){
+			vowels++;
+			
+		}
+
+	}
+	return vowels;
 }
 
 
@@ -24,7 +38,9 @@ int main(){
 	cout << "Enter a word: ";
 	cin >> word;
 
-	cout << endl << countCharacter(word);
 
-	
+
+	cout << endl << "Number of vowels: " << countVowel(word);
+
+	return 0;
 }
